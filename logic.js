1a7c8c9e079cd27d5f6122cc102b83684af4cf64
danielarnost <danielarnost@utexaas.edu>
2017-01-16 22:47:56 -0600

    initial content

--- logic.js
+++ logic.js
@@ -1,1 +1,15 @@
+//global variables
 var gameWords   = ["clapton", "lennon", "johnson", "hendrix", "reinhardt", "vaughan", "berry", "iomi", "richards", "mitchell", "holmes", "raitt"];
+console.log(gameWords);	
+var wins = 0;
+var blanks = 0;
+var gameWord = [];
+var guessesLeft = 9;
+
+game (function(){
+
+
+
+
+
+})
