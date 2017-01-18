f7658e0b8be4f814d80c34d2ca6bfcb0d779d496
danielarnost <danielarnost@utexaas.edu>
2017-01-17 23:00:29 -0600

    logic check Letters func

--- logic.js
+++ logic.js
@@ -1,3 +1,5 @@
+
+
 //global variables
 var gameWords = ["clapton", "lennon", "johnson", "hendrix", "reinhardt", "vaughan", "berry", "iomi", "richards", "mitchell", "holmes", "raitt"];
 // console.log(gameWords);	
@@ -26,11 +28,43 @@ function startGame() {
 	wrongGuesses = [];
 // Fill up the blanksAndSuccesses list with appropriate number of blanks. This is based on number of letters in solution
  for (i = 0; i < gameWords.length; i ++){
+ 		guessBlanks.push("_");
+ }
+console.log(guessBlanks);
+//reprints guesses to 9
+document.getElementById("guessesLeft").innerHTML = guessesLeft;
+document.getElementById("letterBlanks").innerHTML = guessBlanks.join(" ");
+document.getElementById("guessesLeft").innerHTML = wrongGuesses.join(" ");
 
-// }
+}
+// checkLettesr() function
+// It's where we will do all of the comparisons for matches. Again, it's not being called here. It's just being made for future use.
+function checkLetters(letter){
+	//check to make sure guess is a letter and that its lowercase and its in thhe word, used in the future
+var letterInWord = false; // true when in word
 
+// Check if a leter exists inside the array at all.
+	for (var i=0; i<blanks; i++) {
+		if(chosenWord[i] == letter) {
+			letterInWord = true; // if the letter exists then toggle this boolean to true. This will be used in the next step. 
+ 		}
+	}
+// If the letter exists somewhere in the word, then figure out exactly where (which indices)
+	if(letterInWord){
+	
+		// loop through the word 
+		for (var i=0; i<blanks; i++){
 
-
-
-};
-
+			// Populate the guessBlanks with every instance of the letter.
+			if(randomGameWord[i] == letter) {
+				guessBlanks[i] = letter; // here we set the specific space in blanks and letter equal to the letter when there is a match.
+			}
+		}
+		console.log(guessBlanks); // logging for testing
+	}else{
+	wrongGuesses.push(letter); // then we add the letter to the list of wrong letters
+		numGuesses--; // and we subtract one of the guesses
+	}
+}
+// roundComplete() function
+// Here we will have all of the code that needs to be run after each guess is made
